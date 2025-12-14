d2a9ba666fb5a52f9481ee2ef892996f4bd443cd
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-12-15 00:49:38 +0530

    file formatter WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { toast } from "sonner";
 import { useDebounce } from "use-debounce";
 
 export default function Home() {
+	const [file, setFile] = useState<File | null>(null);
 	const [raw, setRaw] = useState<string>(() => {
 		if (typeof window !== "undefined") {
 			return localStorage.getItem("savedJson") ?? "";
@@ -25,12 +26,41 @@ export default function Home() {
 		setFormatted(e.target.value);
 	}
 
-	function copyFormattedJson(){
+	function copyFormattedJson() {
 		if (typeof window !== "undefined" && formatted) {
 			window.navigator.clipboard.writeText(formatted || error);
-			toast("Copied to Clipboard")
+			toast("Copied to Clipboard");
 		}
 		return;
+	}
+
+	async function handleSubmit(e: React.FormEvent) {
+		e.preventDefault();
+		if (!file) return;
+
+		const formData = new FormData();
+		formData.append("jsonFile", file);
+
+		// setLoading(true);
+
+		const res = await fetch("http://localhost:8080/api/json-formatter/format-file", {
+			method: "POST",
+			body: formData,
+		});
+
+		const blob = await res.blob();
+		const url = window.URL.createObjectURL(blob);
+
+		const a = document.createElement("a");
+		a.href = url;
+		a.download = "formatted.json"; // fallback
+		document.body.appendChild(a);
+		a.click();
+		a.remove();
+
+		window.URL.revokeObjectURL(url);
+		// setResult(data);
+		// setLoading(false);
 	}
 
 	// save JSON safely
@@ -61,11 +91,26 @@ export default function Home() {
 
 	return (
 		<div className="flex items-center justify-center p-4 overflow-clip font-semibold text-lg text-off-white">
-			<main className="flex w-full h-[calc(100svh-2rem)] gap-x-4 overflow-clip p-4">
-				<div className="w-1/2 h-full">
+			<main className="flex flex-wrap w-full h-[calc(100svh-2rem)] overflow-clip p-4">
+				<div className="w-full flex justify-center items-center gap-2 mb-4">
+					{/* Hidden real input */}
+					<input id="json-file" type="file" accept=".json" className="hidden" onChange={(e) => setFile(e.target.files?.[0] ?? null)} />
+
+					{/* Fake input */}
+					<label htmlFor="json-file" className="bg-off-white rounded-2xl py-1 px-4 text-black cursor-pointer min-w-max">
+						{file ? file.name : "Select File"}
+					</label>
+
+					{/* Submit button */}
+					<button className="bg-off-white rounded-2xl py-1 px-4 text-black hover:cursor-pointer" onClick={handleSubmit} disabled={!file}>
+						Submit
+					</button>
+				</div>
+
+				<div className="w-1/2 h-full px-2">
 					<textarea value={raw} onChange={updateJsonInput} spellCheck={false} className="h-full w-full bg-background-contrast rounded-3xl p-4 py-5 outline-none resize-none text-nowrap" />
 				</div>
-				<div className="w-1/2 h-full relative">
+				<div className="w-1/2 h-full px-2 relative">
 					<textarea value={error || formatted} onChange={updateFormattedJsonInput} spellCheck={false} className="h-full w-full bg-background-contrast rounded-3xl p-4 py-5 outline-none resize-none text-nowrap" />
 					<button onClick={copyFormattedJson} className="text-white absolute top-4 right-4 hover:cursor-pointer">
 						<FaCopy />
