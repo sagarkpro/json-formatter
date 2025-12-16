424fc69a69cf33579ab7d3c11690908ce8d6432d
Sagar Khatri <sagarkhatri.pro@gmail.com>
2025-12-16 23:04:54 +0530

    added react json view and upload download option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/ui/spinner";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
-import { ChangeEvent, useMemo, useState, useEffect, useLayoutEffect } from "react";
-import { FaCheck, FaCircleXmark, FaCopy, FaCross } from "react-icons/fa6";
+import { useState, useEffect } from "react";
+import { FaCheck,  FaXmark } from "react-icons/fa6";
 import ReactJson from "react-json-view";
 import { toast } from "sonner";
-import { useDebounce } from "use-debounce";
 
 export default function Home() {
 	const githubDarkJsonTheme = {
@@ -45,14 +44,16 @@ export default function Home() {
 	});
 
 	async function handleSubmit(e: React.FormEvent) {
+		setLoading(true);
 		try {
 			e.preventDefault();
 			if (!file) return;
 
 			const formData = new FormData();
 			formData.append("jsonFile", file);
+			formData.append("fileName", file.name);
 
-			const res = await fetch("http://localhost:8080/api/json-formatter/format-file", {
+			const res = await fetch("https://api.cursedshrine.co.in/api/json-formatter/format-file", {
 				method: "POST",
 				body: formData,
 			});
@@ -61,7 +62,7 @@ export default function Home() {
 				const blob = await res.blob();
 				const contentDisposition = res.headers.get("Content-Disposition");
 				console.log("content dis:", contentDisposition, "headers: ", res.headers);
-				
+
 				const filename = contentDisposition ? contentDisposition.split("filename=")[1].replace(/"/g, "") : "processed-file.json";
 
 				// Create download link and trigger download
@@ -88,11 +89,10 @@ export default function Home() {
 		try {
 			const text = await navigator.clipboard.readText();
 			setRaw(text);
-			try{
+			try {
 				const formatted = JSON.parse(text);
 				setFormattedJson(formatted);
-			}
-			catch(err){
+			} catch (err) {
 				setError(err as object);
 				console.log(err);
 			}
@@ -102,9 +102,9 @@ export default function Home() {
 		}
 	};
 
-	function resetStates(){
+	function resetStates() {
 		localStorage.removeItem("savedJson");
-		setRaw(null); 
+		setRaw(null);
 		setFormattedJson(null);
 		setError(null);
 	}
@@ -127,13 +127,14 @@ export default function Home() {
 			<main className="flex flex-wrap w-full overflow-clip p-4">
 				<div className="w-full flex justify-center items-center gap-2 mb-4">
 					{/* Hidden real input */}
-					<input id="json-file" type="file" accept=".json" className="hidden" onChange={(e) => setFile(e.target.files?.[0] ?? null)} />
+					<input disabled={loading} id="json-file" type="file" accept=".json" className="hidden" onChange={(e) => setFile(e.target.files?.[0] ?? null)} />
 
 					<Tooltip>
 						<TooltipTrigger asChild>
 							{/* Fake input */}
-							<label htmlFor="json-file" className="bg-off-white rounded-2xl py-1 px-4 text-black cursor-pointer min-w-max">
+							<label htmlFor="json-file" className="flex items-center gap-x-2 bg-off-white rounded-2xl py-1 px-4 text-black cursor-pointer min-w-max">
 								{file ? file.name : "Upload File"}
+								{loading && <Spinner/>}
 							</label>
 						</TooltipTrigger>
 						<TooltipContent className="bg-off-white rounded-2xl font-semibold">
@@ -144,24 +145,25 @@ export default function Home() {
 					{file && (
 						<div className="w-max flex gap-x-2">
 							{/* Submit button */}
-							<Button className="bg-off-white rounded-full p-2 text-black hover:cursor-pointer" onClick={handleSubmit} disabled={!file}>
+							<button className={`bg-off-white rounded-full p-2 text-black transition ${loading ? "opacity-50 cursor-not-allowed" : "hover:cursor-pointer"}`} onClick={handleSubmit} disabled={loading}>
 								<FaCheck />
-							</Button>
+							</button>
+
 							{/* Cancel button */}
-							<Button className="bg-off-white rounded-full p-2 text-black hover:cursor-pointer" onClick={() => setFile(null)} disabled={!file}>
-								<FaCircleXmark />
-							</Button>
+							<button className={`bg-off-white rounded-full p-2 text-black transition ${loading ? "opacity-50 cursor-not-allowed" : "hover:cursor-pointer"}`} onClick={() => setFile(null)} disabled={loading}>
+								<FaXmark />
+							</button>
 						</div>
 					)}
 				</div>
 
 				<div className="relative w-full h-[calc(100svh-8rem)] rounded-2xl overflow-clip text-base bg-background-contrast p-3">
 					<button onClick={resetStates} className="z-10 absolute top-3 right-3 bg-off-white rounded-full p-2 text-black">
-						<FaCircleXmark />
+						<FaXmark />
 					</button>
 					<div className="w-full h-full overflow-scroll">
-						{(formattedJson || error) ? (
-							<ReactJson src={formattedJson ?? error} theme={githubDarkJsonTheme} enableClipboard={true} displayDataTypes={false} displayObjectSize={false} indentWidth={2} name={false} />
+						{formattedJson || error ? (
+							<ReactJson src={formattedJson ?? error ?? {}} theme={githubDarkJsonTheme} enableClipboard={true} displayDataTypes={false} displayObjectSize={false} indentWidth={2} name={false} />
 						) : (
 							<button onClick={handlePaste} className="w-full h-full flex justify-center items-center">
 								Click To Paste
